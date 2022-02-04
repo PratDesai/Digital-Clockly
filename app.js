4aac85b17821bcfcb56ecb023c75902dc8156391
Pratik Desai <pratik06@gmail.com>
2022-02-03 16:59:09 -0800

    Created the webpage without the tutorial. Updated the clock to stop jitters from the seconds changing. Updated the background pattern.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+
 function updateClock() {
   const dateTime = new Date();
-  const hours =  ('0' + dateTime.getHours()).slice(-2);
-  const minutes = ('0' + dateTime.getUTCMinutes()).slice(-2);
+  const hours = ('0' + dateTime.getHours()).slice(-2);
+  const minutes = ('0' + dateTime.getMinutes()).slice(-2);
   const seconds = ('0' + dateTime.getSeconds()).slice(-2);
   document.getElementById('hours').innerHTML = hours + ':';
   document.getElementById('minutes').innerHTML = minutes + ':';
   document.getElementById('seconds').innerHTML = seconds;
 }
 
-setInterval(updateClock, 100)
+setInterval(updateClock, 100);
